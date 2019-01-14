d9e9f8176729dcaa44af79e10000cb9e56b723cf
kai <kth6rtuo8@gmail.com>
2019-01-14 21:58:04 +0900

    update mysql config info

--- flaskr/config.py
+++ flaskr/config.py
@@ -1,4 +1,4 @@
 #DB関連の接続情報----------------------------------------------
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:human92@localhost/test?charset=utf8'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://user:password@localhost/DBname?charset=utf8'
 SECRET_KEY = 'secret_key'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
